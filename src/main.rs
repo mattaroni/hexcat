8b1a27db1fcd58567b6616b7f6561e10b1685dc0
Matthew Marshall <MatthewMar@proton.me>
2026-06-06 17:20:27 -0500

    Add option to resize each printed line Add option to adjust how many hexadeciml bytes are included in each printed line.

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ mod printer;
 struct Args {
     /// Path of the file to read
     file: String,
+
+    /// Number of bytes to print for each line
+    #[arg(short, long, default_value_t = 27)]
+    width: usize,
 }
 
 fn main() {
     let args = Args::parse();
 
-    if let Err(e) = printer::print_as_hexadecimal(args.file) {
-        eprintln!("\x1b[91;1merror:\x1b[0m {e}");
+    if let Err(e) = printer::print_as_hexadecimal(args.file, args.width) {
+        eprintln!("\x1b[91;1merror:\x1b[0m {}", e.kind());
     }
 }
--- src/printer.rs
+++ src/printer.rs
@@ -1,14 +1,12 @@
 use std::{fs::File, io::{self, BufWriter, Read, Write}};
 
-const CHUNK_SIZE: usize = 27;
-
-pub fn print_as_hexadecimal(filename: String) -> io::Result<()> {
+pub fn print_as_hexadecimal(filename: String, width: usize) -> io::Result<()> {
     let file = File::open(filename)?;
     let mut bytes = file.bytes();
     let mut buffer = BufWriter::new(io::stdout());
 
     loop {
-        let mut chunk = bytes.by_ref().take(CHUNK_SIZE);
+        let mut chunk = bytes.by_ref().take(width);
 
         let first = match chunk.next() {
             Some(x) => x?,
